8551faf7aba5b16b12a1c0b0ed96dc89c8f9d55b
Faris Mohammed <farismosman@gmail.com>
2012-08-20 11:50:36 +0200

    +++++++++++++++++++++++++ about enum ++++++++++++++++++++++++++++ 1. enum is like a classs that defines a set of constants with values associated with. These values are defined using a constructor. The constants do not have to have values, in which case the constructor is optional. 2. you can define methods for the enum "class" as well.

--- koans/src/beginner/AboutEnums.java
+++ koans/src/beginner/AboutEnums.java
@@ -8,38 +8,40 @@ public class AboutEnums {
 
 
 	enum Colors {
-		Red, Blue, Green, Yellow // what happens if you add a ; here?
+		Red(), Blue, Green, Yellow; // what happens if you add a ; here?
 		// What happens if you type Red() instead?
 	}
 	
 	@Koan
 	public void basicEnums() {
 		Colors blue = Colors.Blue;
-		assertEquals(blue == Colors.Blue, __);
-		assertEquals(blue == Colors.Red, __);
-		assertEquals(blue instanceof Colors, __);
+		assertEquals(blue == Colors.Blue, true);
+		assertEquals(blue == Colors.Red, false);
+		assertEquals(blue instanceof Colors, true);
 	}
 	
 	@Koan
 	public void basicEnumsAccess() {
 		Colors[] colorArray = Colors.values();
-		assertEquals(colorArray[2], __);
+		assertEquals(colorArray[2], Colors.Green);
 	}
-	
-	enum SkatSuits {
-		Clubs(12), Spades(11), Hearts(10), Dimonds(9);
-		SkatSuits(int v) { value = v; }
-		private int value;
-	}
-	
-	@Koan
+
+    enum SkatSuits {
+        Clubs(12), Spades(11), Hearts(10), Diamonds(9);
+        SkatSuits(int v) { value = v; }
+        private int value;
+    }
+
+
+    @Koan
 	public void enumsWithAttributes() {
 		// value is private but we still can access it. Why?
 		// Try moving the enum outside the AboutEnum class... What do you expect?
 		// What happens?
-		assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, __);
+		assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, true);
 	}
-	
+
+
 	enum OpticalMedia {
 		CD(650), DVD(4300), BluRay(50000);
 		OpticalMedia(int c) {
@@ -53,7 +55,7 @@ public class AboutEnums {
 	
 	@Koan
 	public void enumsWithMethods() {
-		assertEquals(OpticalMedia.CD.getCoolnessFactor(), __);
-		assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), __);
+		assertEquals(OpticalMedia.CD.getCoolnessFactor(), -3500);
+		assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), 490000);
 	}
 }
